32dbee4c248bd0444e03b31266d03d2107640db7
frankbydefault <franco.hauva@gmail.com>
2022-10-23 00:51:55 -0300

    sd

--- internal/consumer/consumer.go
+++ internal/consumer/consumer.go
@@ -21,10 +21,6 @@ type ConsumerHandler struct {
 	Ready chan bool
 }
 
-type ConsumerHandler struct {
-	Ready chan bool
-}
-
 func (c *ConsumerGroup) Close() error {
 	return c.Consumer.Close()
 }
@@ -113,42 +109,6 @@ func (consumer *ConsumerHandler) ConsumeClaim(session sarama.ConsumerGroupSessio
 		select {
 		case message := <-claim.Messages():
 			log.Printf("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
-			session.MarkMessage(message, "")
-
-		// Should return when `session.Context()` is done.
-		// If not, will raise `ErrRebalanceInProgress` or `read tcp <ip>:<port>: i/o timeout` when kafka rebalance. see:
-		// https://github.com/Shopify/sarama/issues/1192
-		case <-session.Context().Done():
-			return nil
-		}
-	}
-}
-
-func (consumer *ConsumerHandler) Setup(sarama.ConsumerGroupSession) error {
-	// Mark the consumer as ready
-	close(consumer.Ready)
-	return nil
-}
-
-func (consumer *ConsumerHandler) Cleanup(sarama.ConsumerGroupSession) error {
-	return nil
-}
-
-// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
-func (consumer *ConsumerHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-	// NOTE:
-	// Do not move the code below to a goroutine.
-	// The `ConsumeClaim` itself is called within a goroutine, see:
-	// https://github.com/Shopify/sarama/blob/main/consumer_group.go#L27-L29
-	for {
-		select {
-		case message := <-claim.Messages():
-			log.Printf(
-				"Message claimed: value = %s, timestamp = %v, topic = %s",
-				string(message.Value),
-				message.Timestamp,
-				message.Topic,
-			)
 			session.MarkMessage(message, "")
 
 		// Should return when `session.Context()` is done.
